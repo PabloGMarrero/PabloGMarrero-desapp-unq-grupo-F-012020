ad63040607bf3677ff2ab7acf762dbea652fe99d
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-06-10 18:03:57 -0300

    Now the component display the profile component if the user is loggued or display the button login and register if he is not loggued.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-//import { makeStyles } from '@material-ui/core/styles';
+import React, { useState, useContext } from 'react'
 import './Navbar.css';
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
-//import Typography from '@material-ui/core/Typography'
-//import Button from '@material-ui/core/Button'
-//import MenuIcon from '@material-ui/icons/Menu'; 
 import {withRouter } from 'react-router';
 import {Link} from 'react-router-dom';
+import { UserContext} from '../../context/user-context'
 
-class NavBar extends React.Component {
-  constructor(props) {
-      super(props);
+const NavBar = () => {
+  const [user] = useContext(UserContext)
+  console.log(user)
+  const [isLoggued] = useState(user!==null && user !== undefined && user.name!== "")
 
-  }
-  //{coord: {lat: this.props.location.coord.lat , long: this.props.location.coord.lng}
+  return (
+    <AppBar position="static" color="secondary">
+      <Toolbar>
+        <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">     
+          <Link className="nav-link"
+                    to={{pathname: "/"}}>Home</Link>              
+        </IconButton>   
+          <div>{isLoggued ?
+              <Link className="nav-link" to={{pathname: "/perfil"}}>MiPerfil</Link>
+              :
+              <div>
+                <Link className="nav-link" to={{pathname: "/login"}}>Login</Link>
+                <Link className="nav-link" to={{pathname: "/register"}}>Register</Link>
+              </div>
+              }
+          </div>
+                
+      </Toolbar>
+    </AppBar>
+  );
+  
+}
+export default withRouter(NavBar);
 
-  render() {
-      return (
-
-          <AppBar position="static" color="secondary">
+  /*<AppBar position="static" color="secondary">
             <Toolbar>
               <IconButton edge="start" className="menuButton" color="inherit" aria-label="menu">     
                 <Link className="nav-link"
@@ -32,9 +48,4 @@ class NavBar extends React.Component {
                           to={{pathname: "/register", state: {coord: this.props.location.coord}}}>Register</Link>           
             </Toolbar>
           </AppBar>
-
-      );
-    }
-  
-  }
-  export default withRouter(NavBar);
+*/
