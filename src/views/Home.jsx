53393d22bae612db910a4919978ffaaa1c58b8f6
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-06-10 21:58:49 -0300

    deleted unused code, changed class component to functional components

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,19 +4,14 @@ import ContainerHome from '../components/Home/Home'
 import {withRouter} from 'react-router';
 import Footer from '../components/Footer/Footer'
 
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (    
-            <div>
-                 <Navbar></Navbar>
-                 <ContainerHome></ContainerHome>
-                 <Footer></Footer>
-            </div>         
-        )        
-    }
+const Home = () => {
+    return (    
+        <div>
+            <Navbar></Navbar>
+            <ContainerHome></ContainerHome>
+            <Footer></Footer>
+        </div>         
+    )        
 }
 
 export default withRouter(Home);
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 import Location from '../components/Location/Location'
 import Footer from '../components/Footer/Footer'
 
-export default class Root extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (        
-            <div className="containerRoot">
-                <Location ></Location>
-                <Footer></Footer>
-            </div>
-        )
-    }
+const Root = () => {
+    return (        
+        <div className="containerRoot">
+            <Location ></Location>
+            <Footer></Footer>
+        </div>
+    )
 }
 
+export default Root;
