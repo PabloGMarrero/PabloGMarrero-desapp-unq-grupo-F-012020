64c40f9513d791a632807a6d324ca3c742f2123c
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-06-12 14:23:00 -0300

    Refactor location component. Now uses material ui components and text could be translated

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -3,21 +3,45 @@ import { withRouter } from 'react-router';
 import { useHistory } from 'react-router-dom';
 import { CoordenadasContext } from '../../context/location-context'
 import MapboxGLMap from './MapGL.jsx'
+import { useTranslation } from 'react-i18next'
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const styles = makeStyles((theme) => ({
+    root:{
+        margin: theme.spacing(2, 4),
+        display: 'flex',
+        flexFlow: "row-wrap",
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    map:{
+    },
+    button:{
+        color: "#FFFFFF",
+        background: "#B80913",
+    }
+}));
 
 const Location = () =>{
     const [coord] = useContext(CoordenadasContext)
     const image = './assets/img/logoB.png'
     const history = useHistory();
+    const classes = styles();
+    const {t} = useTranslation()
     return (
-        <div>
+        <Box className={classes.root}>
             <div className= "coordenadas">
                 <div> Longitude: {coord.lng} | Latitude: {coord.lat} </div>
             </div>           
-                
-            <button color="inherit" onClick={() => {history.push(`/home`)}}>Buscar productos</button>
             <img src={image} alt={'im'} className="center" />
+            <Button className={classes.button} onClick={() => {history.push(`/home`)}}>{t("Location.SearchProducts")}</Button>
+            
             <MapboxGLMap></MapboxGLMap>
-        </div>
+            
+        </Box>
     )
 }
 
