ab5f0564abe3dfc090e3a6cdbfbb8f2c5fefb84f
PabloGMarrero <pablo.g.marrero@gmail.com>
2020-06-12 14:24:04 -0300

    Home and Root views uses material ui components

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 import Navbar from "../components/Navbar/Navbar";
 import ContainerHome from '../components/Home/Home'
 import {withRouter} from 'react-router';
 import Footer from '../components/Footer/Footer'
 
+const styles = makeStyles((theme) => ({
+    root:{
+        margin: theme.spacing(2, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+}));
+
 const Home = () => {
+    const classes = styles()
     return (    
-        <div>
+        <Box className={classes.root}>
             <Navbar></Navbar>
             <ContainerHome></ContainerHome>
             <Footer></Footer>
-        </div>         
+        </Box>         
     )        
 }
 
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Location from '../components/Location/Location'
 import Footer from '../components/Footer/Footer'
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+
+const styles = makeStyles((theme) => ({
+    root:{
+        margin: theme.spacing(2, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+}));
+
 
 const Root = () => {
-    return (        
-        <div className="containerRoot">
+    const classes = styles()
+    return (    
+        <Box className={classes.root}>
             <Location ></Location>
             <Footer></Footer>
-        </div>
+        </Box>
     )
 }
 
